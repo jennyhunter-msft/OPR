80d8c816fbc9dc9ed5e929f5fb8d34026fc08e27
aquirion <alexandre.j.quirion@gmail.com>
2017-07-24 18:13:36 -0700

    Finally Manage to Ping the Bing Maps API

--- OPRWebApp/OPRWebApp/Controllers/HomeController.cs
+++ OPRWebApp/OPRWebApp/Controllers/HomeController.cs
@@ -1,70 +1,71 @@
 ﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Mime;
+using System.Runtime.Serialization.Json;
+using System.Text;
 using System.Web.Configuration;
 using System.Web.Mvc;
 using BingMapsRESTToolkit;
 using Microsoft.Ajax.Utilities;
+using RestSharp;
 
 namespace OPRWebApp.Controllers
 {
     public class HomeController : Controller
     {
-        private string _bingMapsKey = null;
+        private string _bingMapsKey = WebConfigurationManager.AppSettings["BingMapsKey"];
+        private string _bingMapsRESTUrl = "http://dev.virtualearth.net/REST/v1/Locations?q={0}&key={1}";
 
-        private void Initialize()
+        public ActionResult Index(string sessionId, string location)
         {
-            _bingMapsKey = WebConfigurationManager.AppSettings["BingMapsKey"];
-        }
-
-        public ActionResult Index(string sessionId, string query)
-        {
-            Initialize();
             ViewData["session"] = sessionId;
-            ViewData["BingMapsKey"] = _bingMapsKey;
 
-            if (!string.IsNullOrEmpty(query))
+            if (!string.IsNullOrEmpty(location))
             {
-                ViewData["LocationInformation"] = GetLocationInformation(query);
+                ViewData["LocationName"] = GetLocationName(location);
             }
 
             return View();
         }
 
-        public string GetLocationInformation(string query)
+        public string GetLocationName(string location)
         {
-            string result = string.Format("No results found for query : {0}", query);
-            
-            var response = ServiceManager.GetResponseAsync(new GeocodeRequest()
-            {
-                BingMapsKey = _bingMapsKey,
-                Query = query
-            }).GetAwaiter().GetResult();
-
-            if (response != null && response.ResourceSets != null &&
-                response.ResourceSets.Length > 0 &&
-                response.ResourceSets[0].Resources != null &&
-                response.ResourceSets[0].Resources.Length > 0)
-            {
-                for (var i = 0; i < response.ResourceSets[0].Resources.Length; i++)
-                {
-                    result = (response.ResourceSets[0].Resources[i] as Location).Name;
-                }
-            }
-
+            string result = string.Format("No results found for location : {0}", location);
+            result = GetLocationInformations(location).Name;
             return result;
         }
 
-        public ActionResult About()
+        public string GetLocationCoordinates(string location)
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
+            string result = string.Format("No results found for location : {0}", location);
+            var coordinates = GetLocationInformations(location).Point.Coordinates;
+            result = string.Format("{0}, {1}", coordinates[0], coordinates[1]);
+            return result;
         }
 
-        public ActionResult Contact()
+        private Location GetLocationInformations(string location)
         {
-            ViewBag.Message = "Your contact page.";
+            Uri geoCodeRequest = new Uri(string.Format(_bingMapsRESTUrl, location, _bingMapsKey));
+            return (Location)QueryBingMaps(geoCodeRequest).ResourceSets[0].Resources[0];
+        }
 
-            return View();
+        private Response QueryBingMaps(Uri geoCodeRequest)
+        {
+            var request = (HttpWebRequest) WebRequest.Create(geoCodeRequest);
+
+            using (var response = (HttpWebResponse) request.GetResponse())
+            {
+                if (response.StatusCode != HttpStatusCode.OK)
+                {
+                    var message = String.Format("Request failed. Received HTTP {0}", response.StatusCode);
+                    throw new ApplicationException(message);
+                }
+
+                // grab the response
+                DataContractJsonSerializer ser = new DataContractJsonSerializer(typeof(Response));
+                return ser.ReadObject(response.GetResponseStream()) as Response;
+            }
         }
     }
 }
